7e9b184341bf2cd03b86c2ce9a2938fccaef3796
Artem <bburtart@gmail.com>
2025-10-31 18:36:24 +0200

    1

--- js/myJs.js
+++ js/myJs.js
@@ -9,3 +9,15 @@ select.addEventListener("change", (e) => {
         elem.style.fontFamily = value;
     });
 });
+
+const select2 = document.getElementById('bg');
+
+select2.addEventListener("change", (e) => {
+    const value = e.target.value;
+    console.log(value);
+
+    // Применяем шрифт ко всем элементам
+    document.querySelector(".hero").dataBg=`url('${value}.png')`
+    document.querySelector(".hero").style.backgroundImage=`url('${value}')`
+    console.log(document.querySelector(".hero"))
+});
